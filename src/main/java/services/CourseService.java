b3245662f0e7504fade3ce37b4780836c24baca2
Moatez-000 <moateez.ahmed@gmail.com>
2025-11-15 22:37:04 +0200

    Update CourseService.java

--- src/main/java/services/CourseService.java
+++ src/main/java/services/CourseService.java
@@ -1,7 +1,38 @@
-
 package services;
 
-
+import courses.*;
+import filehandler.*;
+import java.util.ArrayList;
+import users.*;
+import java.io.IOException;
 public class CourseService {
-    
+
+    private UserDataBase userdatabase;
+    private CourseDataBase coursedatabase;
+
+    public CourseService() {
+        userdatabase = new UserDataBase("users.json");
+        coursedatabase = new CourseDataBase("courses.json");
+
+    }
+
+    public void createCourse (String courseId, String title, String description, String intructorId)throws IOException {
+        ArrayList<User> users = userdatabase.returnAllRecords();
+        Course course = new Course();
+        course.setCourseID(courseId);
+        course.setDescription(description);
+        course.setInstructorID(intructorId);
+        course.setTitle(title);
+        for (User user : users) {
+            if (user.getID().equals(intructorId) && user instanceof Instructor) {
+                Instructor instrctor = (Instructor) user;
+                instrctor.addCreatedCourse(courseId);
+            }
+        }
+       userdatabase.write();
+       ArrayList<Course> courses=coursedatabase.returnAllRecords();
+       courses.add(course);
+       coursedatabase.write();
+    }
+
 }
