13687bb71d6f76aac57f7e917597c614de0235fb
mohand <mohandamr2019@gmail.com>
2025-11-15 23:43:33 +0200

    minor

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -1,41 +1,37 @@
-
 package users;
-
-
 
 import misc.Validations;
 
-public abstract class User{
+public abstract class User {
+
     protected String userID;
     protected String username;
     protected String email;
     protected String passwordHash;
-    
-    public User(){
+
+    public User() {
     }
-    
-    public User(String username, String email, String passwordHash){
+
+    public User(String username, String email, String passwordHash) {
         setUsername(username);
         setEmail(email);
         setPasswordHash(passwordHash);
     }
-    
 
     public String getID() {
         return userID;
     }
-    
+
     public abstract void setUserID(String userID);
-   
+
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
-        if(Validations.validateUsername(username) == true){
+        if (Validations.validateUsername(username) == true) {
             this.username = username;
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Invalid username");
         }
     }
@@ -46,10 +42,9 @@ public abstract class User{
 
     public void setEmail(String email) {
         this.email = email;
-        if(Validations.validateEmail(email) == true){
+        if (Validations.validateEmail(email) == true) {
             this.email = email;
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Invalid email");
         }
     }
@@ -59,19 +54,9 @@ public abstract class User{
     }
 
     public void setPasswordHash(String passwordHash) {
-        if(Validations.validatePasswordHash(passwordHash) == true){
-            this.passwordHash = passwordHash;
-        }
-        else{
-            throw new IllegalArgumentException("Invalid password hash, internal hashing error ");
-        }
+
+        this.passwordHash = passwordHash;
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
