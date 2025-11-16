6c24e53e771eb3bd16672ee75d128e5a96be45a7
snmoe <moesnosy2@gmail.com>
2025-11-17 01:11:10 +0200

    Update enrolled_students.java

--- src/main/java/ui/enrolled_students.java
+++ src/main/java/ui/enrolled_students.java
@@ -114,6 +114,7 @@ public class enrolled_students extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
