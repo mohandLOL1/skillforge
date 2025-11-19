e8f0e3bb8196b6c787a0aceeb4870c1c81ce7a2b
snmoe <moesnosy2@gmail.com>
2025-11-19 19:57:23 +0200

    logout

--- src/main/java/ui/admin.java
+++ src/main/java/ui/admin.java
@@ -4,6 +4,8 @@
  */
 package ui;
 
+import java.io.IOException;
+
 /**
  *
  * @author moesn
@@ -32,6 +34,7 @@ public class admin extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         acceptCourse = new javax.swing.JButton();
         declineCourse = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(51, 204, 255));
@@ -63,6 +66,13 @@ public class admin extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("Logout");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -78,6 +88,9 @@ public class admin extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(declineCourse)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jButton1)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -88,7 +101,9 @@ public class admin extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(acceptCourse)
                     .addComponent(declineCourse))
-                .addContainerGap(10, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
         );
 
         pack();
@@ -101,6 +116,18 @@ public class admin extends javax.swing.JFrame {
     private void declineCourseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_declineCourseActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_declineCourseActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+        try {
+            Login lg = new Login();
+            lg.setVisible(true);
+
+        } catch (IOException e) {
+            System.getLogger(InstructorDashboard.class.getName()).log(System.Logger.Level.ERROR, (String) null, e);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -130,6 +157,7 @@ public class admin extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton acceptCourse;
     private javax.swing.JButton declineCourse;
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
