d6e5e22275cd1b9100925a1946c02a546f2b5410
Moatez-000 <moateez.ahmed@gmail.com>
2025-11-23 01:29:44 +0200

    Update AnalyticsService.java

--- src/main/java/services/AnalyticsService.java
+++ src/main/java/services/AnalyticsService.java
@@ -1,15 +1,55 @@
-
 package services;
 
+import courses.*;
+import users.*;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class AnalyticsService {
     //calculateaveragepercourse
     //etc
-    
-     private CourseService courseService;
+
+    private CourseService courseService;
 
     public AnalyticsService(CourseService courseService) {
         this.courseService = courseService;
     }
-    
+
+    public ArrayList<Double> getAverageCourseScore(String courseID) {
+
+        ArrayList<Double> lessonAverages = new ArrayList<>();
+
+        Course course = courseService.findCourse(courseID);
+        ArrayList<Lesson> lessons = courseService.getLessons(courseID);
+        Set<CourseEnrollment> courseenrollments = course.getCourseEnrollments();
+
+        for (Lesson lesson : lessons) {
+
+            double totalScore = 0.0;
+            int attemptCount = 0;
+
+            for (CourseEnrollment enrollment : courseenrollments) {
+
+                ArrayList<StudentQuizAttempt> attempts = enrollment.getQuizAttempts();
+
+                for (StudentQuizAttempt sqa : attempts) {
+
+                    if (lesson.getLessonID().equals(sqa.getLessonID())) {
+                        totalScore += sqa.getScore();
+                        attemptCount++;
+                    }
+                }
+            }
+
+            if (attemptCount > 0) {
+                lessonAverages.add(totalScore / attemptCount);
+            } else {
+
+                lessonAverages.add(0.0);
+            }
+        }
+
+        return lessonAverages;
+    }
+
 }
